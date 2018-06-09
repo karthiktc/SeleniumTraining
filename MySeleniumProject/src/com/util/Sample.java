bcf4b45ab63321ca5d588b12658f8d8289c18809
RS PC <RS PC@192.168.1.6>
2018-06-09 13:08:27 +0530

    vgiuho

--- MySeleniumProject/src/com/util/Sample.java
+++ MySeleniumProject/src/com/util/Sample.java
@@ -2,18 +2,11 @@ package com.util;
 
 public class Sample {
 
-<<<<<<< HEAD
 	public void display()
 	{
 		
 	}
 	
 	
-=======
-	
-	public void display()
-	{
-		
-	}
->>>>>>> branch 'master' of https://github.com/karthiktc/SeleniumTraining.git
+
 }
